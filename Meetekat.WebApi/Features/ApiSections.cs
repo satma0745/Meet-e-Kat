05a53dbf32321efcdfd8490dd91656530dc146b8
satma0745 <catman0745@gmail.com>
2022-04-23 22:05:05 +0300

    Implement the RegisterNewUser feature.

--- Meetekat.WebApi/Features/ApiSections.cs
+++ Meetekat.WebApi/Features/ApiSections.cs
@@ -2,6 +2,7 @@
 
 public static class ApiSections
 {
+    public const string Auth = nameof(Auth);
     public const string HealthChecks = nameof(HealthChecks);
     public const string Meetups = nameof(Meetups);
 }
