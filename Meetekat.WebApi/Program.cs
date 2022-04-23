e9015137cddb11891ae53ef381f58451f4f1e0b3
satma0745 <catman0745@gmail.com>
2022-04-23 14:35:20 +0300

    Add integration with xml comments documentation.

--- Meetekat.WebApi/Program.cs
+++ Meetekat.WebApi/Program.cs
@@ -1,9 +1,21 @@
+using System;
+using System.IO;
+using System.Reflection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    var webApiAssembly = Assembly.GetExecutingAssembly();
+    var webApiProjectName = webApiAssembly.GetName().Name;
+
+    var xmlCommentsFileName = $"{webApiProjectName}.xml";
+    var xmlCommentsFilePath = Path.Combine(AppContext.BaseDirectory, xmlCommentsFileName);
+    
+    options.IncludeXmlComments(xmlCommentsFilePath);
+});
 
 var app = builder.Build();
 app.UseSwagger();
