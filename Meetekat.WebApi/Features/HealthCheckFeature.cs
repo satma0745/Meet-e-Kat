9780af8bc754c0af27054c09143ded84bf01f051
satma0745 <catman0745@gmail.com>
2022-04-23 14:35:56 +0300

    Provide more information about the health check feature.

--- Meetekat.WebApi/Features/HealthCheckFeature.cs
+++ Meetekat.WebApi/Features/HealthCheckFeature.cs
@@ -1,10 +1,15 @@
 namespace Meetekat.WebApi.Features;
 
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 [ApiController]
+[Tags("HealthCheck")]
 public class HealthCheckFeature : ControllerBase
 {
+    /// <summary>Check if application is up and running.</summary>
+    /// <response code="200">The application is working normally.</response>
     [HttpGet("/api/health-checks")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
     public IActionResult Check() => Ok();
 }
