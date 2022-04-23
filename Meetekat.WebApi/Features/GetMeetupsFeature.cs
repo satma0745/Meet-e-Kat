9e707b89699d603f3e0ce249097e42327ed3e5a2
satma0745 <catman0745@gmail.com>
2022-04-23 15:23:23 +0300

    Add GetMeetups and RegisterNewMeetup feature templates.

---
+++ Meetekat.WebApi/Features/GetMeetupsFeature.cs
@@ -0,0 +1,21 @@
+﻿namespace Meetekat.WebApi.Features;
+
+using System;
+using System.Collections.Generic;
+using System.Net.Mime;
+using Meetekat.WebApi.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+[ApiController]
+[Tags("Meetups")]
+[Produces(MediaTypeNames.Application.Json)]
+public class GetMeetupsFeature : ControllerBase
+{
+    [HttpGet("/api/meetups")]
+    [SwaggerOperation("Get all meetups.")]
+    [SwaggerResponse(StatusCodes.Status200OK, "Meetups retrieved successfully.", typeof(IEnumerable<Meetup>))]
+    public IActionResult GetMeetups() =>
+        throw new NotImplementedException();
+}
---
+++ Meetekat.WebApi/Features/RegisterNewMeetupFeature.cs
@@ -0,0 +1,21 @@
+﻿namespace Meetekat.WebApi.Features;
+
+using System;
+using System.Net.Mime;
+using Meetekat.WebApi.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+[ApiController]
+[Tags("Meetups")]
+[Consumes(MediaTypeNames.Application.Json)]
+[Produces(MediaTypeNames.Application.Json)]
+public class RegisterNewMeetupFeature : ControllerBase
+{
+    [HttpPost("/api/meetups")]
+    [SwaggerOperation("Register a new meetup.")]
+    [SwaggerResponse(StatusCodes.Status201Created, "A new meetup is registered successfully.", typeof(Meetup))]
+    public IActionResult RegisterNewMeetup([FromBody] Meetup meetup) =>
+        throw new NotImplementedException();
+}
