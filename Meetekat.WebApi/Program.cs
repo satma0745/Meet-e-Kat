f4a0a88a710cbaf14445b57a0ca7932a1c297072
satma0745 <catman0745@gmail.com>
2022-04-23 14:28:21 +0300

    Enable Swagger.

--- Meetekat.WebApi/Program.cs
+++ Meetekat.WebApi/Program.cs
@@ -3,7 +3,10 @@ using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+app.UseSwagger();
+app.UseSwaggerUI();
 app.MapControllers();
 app.Run();
