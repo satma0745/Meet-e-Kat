8f90de4f9999c8d1e1b3a7a1405f923cfbeba146
satma0745 <54468544+satma0745@users.noreply.github.com>
2022-04-23 15:09:05 +0300

    Add swagger support (#1)

--- Meetekat.WebApi/Features/HealthCheckFeature.cs
+++ Meetekat.WebApi/Features/HealthCheckFeature.cs
@@ -1,10 +1,15 @@
 namespace Meetekat.WebApi.Features;
 
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
 
 [ApiController]
+[Tags("HealthCheck")]
 public class HealthCheckFeature : ControllerBase
 {
     [HttpGet("/api/health-checks")]
+    [SwaggerOperation("Check if application is up and running.")]
+    [SwaggerResponse(StatusCodes.Status200OK, "The application is working normally.")]
     public IActionResult Check() => Ok();
 }
--- Meetekat.WebApi/Program.cs
+++ Meetekat.WebApi/Program.cs
@@ -1,9 +1,16 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Annotations;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.OperationFilter<AnnotationsOperationFilter>();
+});
 
 var app = builder.Build();
+app.UseSwagger();
+app.UseSwaggerUI();
 app.MapControllers();
 app.Run();
