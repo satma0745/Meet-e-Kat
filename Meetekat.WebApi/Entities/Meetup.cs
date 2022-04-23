fa0e8039dc8a6728295f7d2b0f8615b32b91ed66
satma0745 <catman0745@gmail.com>
2022-04-23 15:15:47 +0300

    Add meetup entity.

---
+++ Meetekat.WebApi/Entities/Meetup.cs
@@ -0,0 +1,21 @@
+﻿namespace Meetekat.WebApi.Entities;
+
+using System;
+using System.Collections.Generic;
+
+public class Meetup
+{
+    public Guid Id { get; set; }
+    
+    public string Title { get; set; }
+
+    public string Description { get; set; }
+    
+    public ICollection<string> Tags { get; set; }
+    
+    public DateTime StartTime { get; set; }
+    
+    public DateTime EndTime { get; set; }
+    
+    public string Organizer { get; set; }
+}
