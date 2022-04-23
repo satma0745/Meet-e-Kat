5fa024cf93932d6d0e8bc5f94c1c9c7bd27d9f86
satma0745 <catman0745@gmail.com>
2022-04-23 23:16:19 +0300

    Configure JwtBearer authentication + implement the GetCurrentUserInfo feature/

---
+++ Meetekat.WebApi/Features/Auth/GetCurrentUserInfo/GetCurrentUserInfoFeature.cs
@@ -0,0 +1,43 @@
+﻿namespace Meetekat.WebApi.Features.Auth.GetCurrentUserInfo;
+
+using System;
+using System.Linq;
+using System.Security.Claims;
+using Meetekat.WebApi.Persistence;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+public class GetCurrentUserInfoFeature : FeatureBase
+{
+    private readonly ApplicationContext context;
+
+    public GetCurrentUserInfoFeature(ApplicationContext context) =>
+        this.context = context;
+
+    [Tags(ApiSections.Auth)]
+    [HttpGet("/api/users/self")]
+    [Authorize]
+    [SwaggerOperation("Get current User info.")]
+    [SwaggerResponse(StatusCodes.Status200OK, "Current User info retrieved successfully.", typeof(CurrentUserDto))]
+    public IActionResult GetCurrentUserInfo()
+    {
+        var currentUserIdClaim = User.Claims.Single(claim => claim.Type == ClaimTypes.NameIdentifier);
+        var currentUserId = Guid.Parse(currentUserIdClaim.Value);
+
+        var currentUser = context.Users.SingleOrDefault(user => user.Id == currentUserId);
+        if (currentUser is null)
+        {
+            // This can happen when User is deleted, but his token is still not expired.
+            return Unauthorized();
+        }
+
+        var currentUserDto = new CurrentUserDto
+        {
+            Id = currentUser.Id,
+            Username = currentUser.Username
+        };
+        return Ok(currentUserDto);
+    }
+}
--- Meetekat.WebApi/Program.cs
+++ Meetekat.WebApi/Program.cs
@@ -1,11 +1,14 @@
 using System;
 using System.IO;
 using System.Reflection;
+using System.Text;
 using Meetekat.WebApi.Auth;
 using Meetekat.WebApi.Persistence;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 using Swashbuckle.AspNetCore.Annotations;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -25,9 +28,41 @@ builder.Services.AddDbContext<ApplicationContext>(options =>
     options.UseInMemoryDatabase("In-Memory DB");
 });
 builder.Services.AddSingleton<JwtTokenService>();
+builder.Services
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        // TODO: Refactor Auth configuration.
+
+        const string secretKeyParameterName = "Auth:SecretKey";
+        var secretKeyParameter = builder.Configuration[secretKeyParameterName];
+        if (string.IsNullOrWhiteSpace(secretKeyParameter))
+        {
+            throw new Exception($"Configuration parameter \"{secretKeyParameterName}\" is required.");
+        }
+        var secretKeyBytes = Encoding.UTF8.GetBytes(secretKeyParameter);
+        var secretKey = new SymmetricSecurityKey(secretKeyBytes);
+
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            RequireSignedTokens = true,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = secretKey,
+            
+            ValidateAudience = false,
+            ValidateIssuer = false,
+            
+            RequireExpirationTime = true,
+            ValidateLifetime = true,
+            ClockSkew = TimeSpan.Zero
+        };
+        options.RequireHttpsMetadata = false;
+    });
 
 var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI();
+app.UseAuthentication();
+app.UseAuthorization();
 app.MapControllers();
 app.Run();
