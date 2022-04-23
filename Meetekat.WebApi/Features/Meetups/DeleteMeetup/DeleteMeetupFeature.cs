912f3fd08dba86573a0a0b661a187ab9cc3d9bbb
satma0745 <catman0745@gmail.com>
2022-04-23 18:48:13 +0300

    Implement the DeleteMeetup feature.

---
+++ Meetekat.WebApi/Features/Meetups/DeleteMeetup/DeleteMeetupFeature.cs
@@ -0,0 +1,36 @@
+﻿namespace Meetekat.WebApi.Features.Meetups.DeleteMeetup;
+
+using System;
+using System.Linq;
+using Meetekat.WebApi.Persistence;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+[ApiController]
+[Tags("Meetups")]
+public class DeleteMeetupFeature : ControllerBase
+{
+    private readonly ApplicationContext context;
+
+    public DeleteMeetupFeature(ApplicationContext context) =>
+        this.context = context;
+
+    [HttpGet("/api/meetups/{meetupId:guid}")]
+    [SwaggerOperation("Delete a Meetup with matching ID.")]
+    [SwaggerResponse(StatusCodes.Status204NoContent, "A Meetup with the specified ID was deleted successfully.")]
+    [SwaggerResponse(StatusCodes.Status404NotFound, "A Meetup with the specified ID doesn't exist.")]
+    public IActionResult DeleteMeetup([FromRoute] Guid meetupId)
+    {
+        var meetup = context.Meetups.SingleOrDefault(meetup => meetup.Id == meetupId);
+        if (meetup is null)
+        {
+            return NotFound();
+        }
+
+        context.Meetups.Remove(meetup);
+        context.SaveChanges();
+
+        return NoContent();
+    }
+}
