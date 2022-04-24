d02ad213231ce08b7d9365717beeaa52caf9a949
satma0745 <catman0745@gmail.com>
2022-04-24 11:53:44 +0300

    Update the ChangeCredentials feature implementation so that only the User himself can change his own credentials and no one else can.

--- Meetekat.WebApi/Features/Auth/ChangeCredentials/ChangeCredentialsFeature.cs
+++ Meetekat.WebApi/Features/Auth/ChangeCredentials/ChangeCredentialsFeature.cs
@@ -2,8 +2,10 @@
 
 using System;
 using System.Linq;
+using System.Security.Claims;
 using BCrypt.Net;
 using Meetekat.WebApi.Persistence;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -17,16 +19,20 @@ public class ChangeCredentialsFeature : FeatureBase
         this.context = context;
 
     [Tags(ApiSections.Auth)]
-    [HttpPut("/api/users/{userId:guid}/credentials")]
+    [HttpPut("/api/users/self/credentials")]
+    [Authorize]
     [SwaggerOperation("Change signing credentials for a User with the specified ID.")]
     [SwaggerResponse(StatusCodes.Status200OK, "Signing credentials were updated successfully.")]
     [SwaggerResponse(StatusCodes.Status404NotFound, "User with the specified ID doesn't exist.")]
     [SwaggerResponse(StatusCodes.Status409Conflict, "Specified username is already taken by some other user.")]
-    public IActionResult ChangeCredentials([FromRoute] Guid userId, [FromBody] ChangeCredentialsDto credentialsDto)
+    public IActionResult ChangeCredentials([FromBody] ChangeCredentialsDto credentialsDto)
     {
+        var currentUserIdClaim = User.Claims.Single(claim => claim.Type == ClaimTypes.NameIdentifier);
+        var currentUserId = Guid.Parse(currentUserIdClaim.Value);
+        
         var user = context.Users
             .Include(user => user.RefreshTokens)
-            .SingleOrDefault(user => user.Id == userId);
+            .SingleOrDefault(user => user.Id == currentUserId);
         if (user is null)
         {
             return NotFound();
@@ -35,7 +41,7 @@ public class ChangeCredentialsFeature : FeatureBase
         // In this query we're excluding target User. This way User can preserve the Username
         // and only change the Password (without getting Username uniqueness violation error). 
         var usernameAlreadyTaken = context.Users
-            .Where(anotherUser => anotherUser.Id != userId)
+            .Where(anotherUser => anotherUser.Id != user.Id)
             .Any(anotherUser => anotherUser.Username == credentialsDto.Username);
         if (usernameAlreadyTaken)
         {
